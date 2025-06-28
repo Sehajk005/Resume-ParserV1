40fc2c165e74791ba8b84cd6d2f02a048d2d3a24
Sehajk005 <sehajk2048@gmail.com>
2025-06-28 14:08:00 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,12 +3,7 @@ import os
 from docx import Document
 import spacy
 import spacy
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 import re
 # Function to extract text from a PDF or DOCX file
 def extract_text(file_path: str) -> str:
